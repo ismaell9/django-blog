65b2e0ad5e25a129930a992794953fe0077817f7
reputationx <muhammad.a.esmael@gmail.com>
2019-06-09 19:58:00 +0200

    user register form

--- blog/django_project/settings.py
+++ blog/django_project/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
+    
 ]
 
 MIDDLEWARE = [
@@ -119,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
