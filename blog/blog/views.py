6f91638741362aacec1c538bb0f75e34fd1b92fc
reputationx <muhammad.a.esmael@gmail.com>
2019-06-09 18:45:54 +0200

    update model

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author' : 'Corut',
-        'title' : 'Blog post 1',
-        'content' : 'first blog post',
-        'date_posted' : 'Augest 20, 2019'
-    },
-    {
-        'author' : 'Gwin',
-        'title' : 'Blog post 2',
-        'content' : 'Secone blog post',
-        'date_posted' : 'Augest 25, 2019'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context=context)
 
